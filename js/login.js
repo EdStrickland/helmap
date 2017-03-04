61694950b48aee1275d2886947915eea509bbaa4
EdStrickland <2499128545@qq.com>
2017-03-04 21:09:50 +0800

    js

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 			url: 'http://115.28.101.55:3000/user/login',
 			data: {
 				'id': '10142510110',
-				'pwd': 'KQ$(f62k';
+				'pwd': 'KQ$(f62k'
 			}
 			success: function(data){
 				alert(data.ret);
